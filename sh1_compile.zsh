868a132a4b2d7bbfe6fac2a4c440ba12d4648a8f
chenxu <chenxu@mail.ustc.edu.cn>
2023-06-22 18:44:08 +0800

    add jobs

--- sh1_compile.zsh
+++ sh1_compile.zsh
@@ -1,25 +1,31 @@
-#!/bin/zsh
-# copyright https://mit-license.org
-# File              : sh1_compile.sh
-# Author            : chenxu <chenxu@mail.ustc.edu.cn>
-# Date              : 2 2021-06-08 16:26:06
-# Last Modified Date: 2 2021-06-08 16:26:06
-# Last Modified By  : chenxu <chenxu@mail.ustc.edu.cn>
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    sh1_compile.zsh                                    :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: chenxu <chenxu@mail.ustc.edu.cn>           +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/06/08 18:43:25 by chenxu            #+#    #+#              #
+#    Updated: 2023/06/22 18:44:04 by chenxu           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+jobs=6
 
 cd ${HOME}/software/corsika/corsikainterface/build/
 rm -rf ${HOME}/software/corsika/corsikainterface/build/*
 cmake ../git
-make -j2
+make -j${jobs}
 make install
 
 cd ${HOME}/software/coord_cx/build
 rm -rf ${HOME}/software/coord_cx/build/*
 cmake ../git
-make -j2
+make -j${jobs}
 make install
 
 cd ${HOME}/software/ROOT/ROOT_cx2/build
 rm -rf ${HOME}/software/ROOT/ROOT_cx2/build/*
 cmake ../git
-make -j2
+make -j${jobs}
 make install
