a2b4b9853ddfca0add4137b08a854f055c6776ef
chenxu <chenxu@mail.ustc.edu.cn>
2023-07-04 17:12:30 +0800

    mv ROOT_cx2

--- sh1_compile.zsh
+++ sh1_compile.zsh
@@ -6,7 +6,7 @@
 #    By: chenxu <chenxu@mail.ustc.edu.cn>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/08 18:43:25 by chenxu            #+#    #+#              #
-#    Updated: 2023/06/22 18:44:04 by chenxu           ###   ########.fr        #
+#    Updated: 2023/07/04 17:12:23 by chenxu           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -24,8 +24,8 @@ cmake ../git
 make -j${jobs}
 make install
 
-cd ${HOME}/software/ROOT/ROOT_cx2/build
-rm -rf ${HOME}/software/ROOT/ROOT_cx2/build/*
+cd ${HOME}/software/chenxufunc/build
+rm -rf ${HOME}/software/chenxufunc/build/*
 cmake ../git
 make -j${jobs}
 make install
