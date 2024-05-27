643a01e00ae30f899708eb21913933c2173ce0e2
chenxu <chenxu@mail.ustc.edu.cn>
2024-05-27 14:12:38 +0800

    ninja faster

--- sh1_compile.zsh
+++ sh1_compile.zsh
@@ -6,26 +6,26 @@
 #    By: chenxu <chenxu@mail.ustc.edu.cn>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/08 18:43:25 by chenxu            #+#    #+#              #
-#    Updated: 2023/07/04 17:12:23 by chenxu           ###   ########.fr        #
+#    Updated: 2024/05/27 13:20:47 by chenxu           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
-
+# cspell:disable
 jobs=6
 
 cd ${HOME}/software/corsika/corsikainterface/build/
 rm -rf ${HOME}/software/corsika/corsikainterface/build/*
-cmake ../git
-make -j${jobs}
-make install
+cmake ../git -GNinja
+ninja
+ninja install
 
 cd ${HOME}/software/coord_cx/build
 rm -rf ${HOME}/software/coord_cx/build/*
-cmake ../git
-make -j${jobs}
-make install
+cmake ../git -GNinja
+ninja
+ninja install
 
 cd ${HOME}/software/chenxufunc/build
 rm -rf ${HOME}/software/chenxufunc/build/*
-cmake ../git
-make -j${jobs}
-make install
+cmake ../git -GNinja
+ninja
+ninja install
